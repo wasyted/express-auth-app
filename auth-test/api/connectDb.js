b867d7aec76f35f240d00233074847c71b2aea91
Matías Ezequiel Wasyluk <87504408+wasyted@users.noreply.github.com>
2024-02-15 21:27:17 -0300

    fixes friend buttons

--- auth-test/api/connectDb.js
+++ auth-test/api/connectDb.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
+mongoose.set("strictQuery", false);
+const dev_db_url = "mongodb+srv://laboratorio:4ntivar1@cluster0.akap3xz.mongodb.net/?retryWrites=true&w=majority";
 
-const mongoDb = "mongodb+srv://laboratorio:4ntivar1@cluster0.akap3xz.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
-mongoose.connect(mongoDb);
+mongoose.connect(mongoDB);
 
 const db = mongoose.connection;
 
